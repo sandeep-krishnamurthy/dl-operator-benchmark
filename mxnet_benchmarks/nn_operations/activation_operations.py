69555c8ebb9b15e3a9552feb685029d2470ea256
Sandeep Krishnamurthy <sandeep.krishna98@gmail.com>
2019-04-24 18:53:46 -0700

    Add benchmarks for Gluon Activations - sigmoid, softmax, log_softmax, tanh, selu, elu, swish

--- mxnet_benchmarks/nn_operations/activation_operations.py
+++ mxnet_benchmarks/nn_operations/activation_operations.py
@@ -4,20 +4,23 @@ from mxnet.gluon import nn
 
 from mxnet_benchmarks.MXNetOperatorBenchmark import MXNetOperatorBenchmarkBase
 from mxnet_benchmarks.utils.gluon_utils import block_forward_backward_and_time
-from mxnet_benchmarks.utils.ndarray_utils import get_mx_ndarray
+from mxnet_benchmarks.utils.ndarray_utils import get_mx_ndarray, nd_forward_backward_and_time
 
 """ Performance benchmark Tests for MXNet Gluon Activation Layers.
 1. LeakyRelu
 2. PRelu
-3. Sigmoid
-4. Softmax
-5. Log_Softmax
-6. Activation
+3. Activation (Sigmoid)
+4. Activation (Softmax) (TODO - GLUON does not have Softmax block, using NDArray APIs for now)
+5. Activation (Log_Softmax) (TODO - GLUON does not have Log_Softmax block, using NDArray APIs for now)
+6. Activation (tanh)
+7. Elu
+8. Selu
+9. Swish
 """
 
 
 class LeakyRelu(MXNetOperatorBenchmarkBase):
-    """Helps to benchmark Gluon LeakyRelu Block.
+    """Helps to benchmark Gluon LeakyRelu Activation Block.
 
     By default, benchmarks both forward and backward pass on the LeakyRelu block on (32, 3, 256, 256) input with
     'float32' precision. Use alpha=0.01.
@@ -56,7 +59,7 @@ class LeakyRelu(MXNetOperatorBenchmarkBase):
 
 
 class PRelu(MXNetOperatorBenchmarkBase):
-    """Helps to benchmark Gluon PRelu Block.
+    """Helps to benchmark Gluon PRelu Activation Block.
 
     By default, benchmarks both forward and backward pass on the PRelu block on (32, 3, 256, 256) input with
     'float32' precision. Use zero initializer to initialize alpha (Parameter of shape same as input and value
@@ -95,6 +98,264 @@ class PRelu(MXNetOperatorBenchmarkBase):
         self.results["MX_Gluon_Imperative_PRelu_Forward_Backward_Time"] = exe_time / self.runs
 
 
+class Sigmoid(MXNetOperatorBenchmarkBase):
+    """Helps to benchmark Gluon Sigmoid Activation Block.
+
+    By default, benchmarks both forward and backward pass on the Sigmoid Activation block on (32, 3, 256, 256) input
+    with 'float32' precision.
+
+    """
+
+    def __init__(self, ctx=mx.cpu(), warmup=10, runs=50, inputs=None):
+        # Set the default Inputs.
+        # Default data is (32, 3, 256, 256) to mimic an input of batch_size=128 and a sample image of size 3*256*256.
+        if inputs is None:
+            inputs = {"data": (32, 3, 256, 256),
+                      "data_initializer": nd.normal,
+                      "run_backward": True,
+                      "dtype": "float32"}
+
+        super().__init__(ctx=ctx, warmup=warmup, runs=runs, inputs=inputs)
+
+        self.data = get_mx_ndarray(ctx=self.ctx, in_tensor=self.inputs["data"],
+                                   dtype=self.inputs["dtype"],
+                                   initializer=self.inputs["data_initializer"],
+                                   attach_grad=self.inputs["run_backward"])
+
+        self.block = nn.Activation(activation="sigmoid")
+
+        self.block.initialize(ctx=self.ctx)
+
+    def run_benchmark(self):
+        # Warm up, ignore execution time value
+        _, _ = block_forward_backward_and_time(block=self.block, runs=self.warmup, x=self.data)
+
+        # Run Benchmarks
+        exe_time, _ = block_forward_backward_and_time(block=self.block, runs=self.runs, x=self.data)
+
+        self.results["MX_Gluon_Imperative_Sigmoid_Forward_Backward_Time"] = exe_time / self.runs
+
+
+class Softmax(MXNetOperatorBenchmarkBase):
+    """Helps to benchmark NDArray Softmax Activation (GLUON doesnot have a softmax block!).
+
+    By default, benchmarks both forward and backward pass on the NDArray Softmax operation on (32, 3, 256, 256) input
+    with 'float32' precision.
+
+    """
+
+    def __init__(self, ctx=mx.cpu(), warmup=10, runs=50, inputs=None):
+        # Set the default Inputs.
+        # Default data is (32, 3, 256, 256) to mimic an input of batch_size=128 and a sample image of size 3*256*256.
+        if inputs is None:
+            inputs = {"data": (32, 3, 256, 256),
+                      "data_initializer": nd.normal,
+                      "run_backward": True,
+                      "dtype": "float32"}
+
+        super().__init__(ctx=ctx, warmup=warmup, runs=runs, inputs=inputs)
+
+        self.data = get_mx_ndarray(ctx=self.ctx, in_tensor=self.inputs["data"],
+                                   dtype=self.inputs["dtype"],
+                                   initializer=self.inputs["data_initializer"],
+                                   attach_grad=self.inputs["run_backward"])
+
+    def run_benchmark(self):
+        # Warm up, ignore execution time value
+        _, _ = nd_forward_backward_and_time(F=nd.softmax, runs=self.warmup, data=self.data)
+
+        # Run Benchmarks
+        exe_time, _ = nd_forward_backward_and_time(F=nd.softmax, runs=self.runs, data=self.data)
+
+        self.results["MX_Softmax_Forward_Backward_Time"] = exe_time / self.runs
+
+
+class LogSoftmax(MXNetOperatorBenchmarkBase):
+    """Helps to benchmark NDArray Log_Softmax Activation (GLUON does not have a log_softmax block!).
+
+    By default, benchmarks both forward and backward pass on the Log_Softmax Activation operation on (32, 3, 256, 256)
+    input with 'float32' precision.
+
+    """
+
+    def __init__(self, ctx=mx.cpu(), warmup=10, runs=50, inputs=None):
+        # Set the default Inputs.
+        # Default data is (32, 3, 256, 256) to mimic an input of batch_size=128 and a sample image of size 3*256*256.
+        if inputs is None:
+            inputs = {"data": (32, 3, 256, 256),
+                      "data_initializer": nd.normal,
+                      "run_backward": True,
+                      "dtype": "float32"}
+
+        super().__init__(ctx=ctx, warmup=warmup, runs=runs, inputs=inputs)
+
+        self.data = get_mx_ndarray(ctx=self.ctx, in_tensor=self.inputs["data"],
+                                   dtype=self.inputs["dtype"],
+                                   initializer=self.inputs["data_initializer"],
+                                   attach_grad=self.inputs["run_backward"])
+
+    def run_benchmark(self):
+        # Warm up, ignore execution time value
+        _, _ = nd_forward_backward_and_time(F=nd.log_softmax, runs=self.warmup, data=self.data)
+
+        # Run Benchmarks
+        exe_time, _ = nd_forward_backward_and_time(F=nd.log_softmax, runs=self.runs, data=self.data)
+
+        self.results["MX_Log_Softmax_Forward_Backward_Time"] = exe_time / self.runs
+
+
+class Tanh(MXNetOperatorBenchmarkBase):
+    """Helps to benchmark Gluon Tanh Activation Block.
+
+    By default, benchmarks both forward and backward pass on the Tanh Activation block on (32, 3, 256, 256) input
+    with 'float32' precision.
+
+    """
+
+    def __init__(self, ctx=mx.cpu(), warmup=10, runs=50, inputs=None):
+        # Set the default Inputs.
+        # Default data is (32, 3, 256, 256) to mimic an input of batch_size=128 and a sample image of size 3*256*256.
+        if inputs is None:
+            inputs = {"data": (32, 3, 256, 256),
+                      "data_initializer": nd.normal,
+                      "run_backward": True,
+                      "dtype": "float32"}
+
+        super().__init__(ctx=ctx, warmup=warmup, runs=runs, inputs=inputs)
+
+        self.data = get_mx_ndarray(ctx=self.ctx, in_tensor=self.inputs["data"],
+                                   dtype=self.inputs["dtype"],
+                                   initializer=self.inputs["data_initializer"],
+                                   attach_grad=self.inputs["run_backward"])
+
+        self.block = nn.Activation(activation="tanh")
+
+        self.block.initialize(ctx=self.ctx)
+
+    def run_benchmark(self):
+        # Warm up, ignore execution time value
+        _, _ = block_forward_backward_and_time(block=self.block, runs=self.warmup, x=self.data)
+
+        # Run Benchmarks
+        exe_time, _ = block_forward_backward_and_time(block=self.block, runs=self.runs, x=self.data)
+
+        self.results["MX_Gluon_Imperative_Tanh_Forward_Backward_Time"] = exe_time / self.runs
+
+
+class Elu(MXNetOperatorBenchmarkBase):
+    """Helps to benchmark Gluon Elu Activation Block.
+
+    By default, benchmarks both forward and backward pass on the Elu Activation block on (32, 3, 256, 256) input
+    with 'float32' precision.
+
+    """
+
+    def __init__(self, ctx=mx.cpu(), warmup=10, runs=50, inputs=None):
+        # Set the default Inputs.
+        # Default data is (32, 3, 256, 256) to mimic an input of batch_size=128 and a sample image of size 3*256*256.
+        if inputs is None:
+            inputs = {"data": (32, 3, 256, 256),
+                      "data_initializer": nd.normal,
+                      "run_backward": True,
+                      "dtype": "float32"}
+
+        super().__init__(ctx=ctx, warmup=warmup, runs=runs, inputs=inputs)
+
+        self.data = get_mx_ndarray(ctx=self.ctx, in_tensor=self.inputs["data"],
+                                   dtype=self.inputs["dtype"],
+                                   initializer=self.inputs["data_initializer"],
+                                   attach_grad=self.inputs["run_backward"])
+
+        self.block = nn.ELU()
+
+        self.block.initialize(ctx=self.ctx)
+
+    def run_benchmark(self):
+        # Warm up, ignore execution time value
+        _, _ = block_forward_backward_and_time(block=self.block, runs=self.warmup, x=self.data)
+
+        # Run Benchmarks
+        exe_time, _ = block_forward_backward_and_time(block=self.block, runs=self.runs, x=self.data)
+
+        self.results["MX_Gluon_Imperative_Elu_Forward_Backward_Time"] = exe_time / self.runs
+
+
+class Selu(MXNetOperatorBenchmarkBase):
+    """Helps to benchmark Gluon Selu Activation Block.
+
+    By default, benchmarks both forward and backward pass on the Selu Activation block on (32, 3, 256, 256) input
+    with 'float32' precision.
+
+    """
+
+    def __init__(self, ctx=mx.cpu(), warmup=10, runs=50, inputs=None):
+        # Set the default Inputs.
+        # Default data is (32, 3, 256, 256) to mimic an input of batch_size=128 and a sample image of size 3*256*256.
+        if inputs is None:
+            inputs = {"data": (32, 3, 256, 256),
+                      "data_initializer": nd.normal,
+                      "run_backward": True,
+                      "dtype": "float32"}
+
+        super().__init__(ctx=ctx, warmup=warmup, runs=runs, inputs=inputs)
+
+        self.data = get_mx_ndarray(ctx=self.ctx, in_tensor=self.inputs["data"],
+                                   dtype=self.inputs["dtype"],
+                                   initializer=self.inputs["data_initializer"],
+                                   attach_grad=self.inputs["run_backward"])
+
+        self.block = nn.SELU()
+
+        self.block.initialize(ctx=self.ctx)
+
+    def run_benchmark(self):
+        # Warm up, ignore execution time value
+        _, _ = block_forward_backward_and_time(block=self.block, runs=self.warmup, x=self.data)
+
+        # Run Benchmarks
+        exe_time, _ = block_forward_backward_and_time(block=self.block, runs=self.runs, x=self.data)
+
+        self.results["MX_Gluon_Imperative_Selu_Forward_Backward_Time"] = exe_time / self.runs
+
+
+class Swish(MXNetOperatorBenchmarkBase):
+    """Helps to benchmark Gluon Swish Activation Block.
+
+    By default, benchmarks both forward and backward pass on the Swish Activation block on (32, 3, 256, 256) input
+    with 'float32' precision.
+
+    """
+
+    def __init__(self, ctx=mx.cpu(), warmup=10, runs=50, inputs=None):
+        # Set the default Inputs.
+        # Default data is (32, 3, 256, 256) to mimic an input of batch_size=128 and a sample image of size 3*256*256.
+        if inputs is None:
+            inputs = {"data": (32, 3, 256, 256),
+                      "data_initializer": nd.normal,
+                      "run_backward": True,
+                      "dtype": "float32"}
+
+        super().__init__(ctx=ctx, warmup=warmup, runs=runs, inputs=inputs)
+
+        self.data = get_mx_ndarray(ctx=self.ctx, in_tensor=self.inputs["data"],
+                                   dtype=self.inputs["dtype"],
+                                   initializer=self.inputs["data_initializer"],
+                                   attach_grad=self.inputs["run_backward"])
+
+        self.block = nn.Swish()
+
+        self.block.initialize(ctx=self.ctx)
+
+    def run_benchmark(self):
+        # Warm up, ignore execution time value
+        _, _ = block_forward_backward_and_time(block=self.block, runs=self.warmup, x=self.data)
+
+        # Run Benchmarks
+        exe_time, _ = block_forward_backward_and_time(block=self.block, runs=self.runs, x=self.data)
+
+        self.results["MX_Gluon_Imperative_Swish_Forward_Backward_Time"] = exe_time / self.runs
+
+
 # Utilities
 def run_all_gluon_nn_basic_operations_benchmarks():
     """Helper to run all Gluon Activation Layer benchmarks. Just runs the benchmarks with default input values.
@@ -110,5 +371,32 @@ def run_all_gluon_nn_basic_operations_benchmarks():
     benchmark_ref.run_benchmark()
     benchmark_ref.print_benchmark_results()
 
+    benchmark_ref = Sigmoid()
+    benchmark_ref.run_benchmark()
+    benchmark_ref.print_benchmark_results()
+
+    benchmark_ref = Softmax()
+    benchmark_ref.run_benchmark()
+    benchmark_ref.print_benchmark_results()
+
+    benchmark_ref = LogSoftmax()
+    benchmark_ref.run_benchmark()
+    benchmark_ref.print_benchmark_results()
+
+    benchmark_ref = Tanh()
+    benchmark_ref.run_benchmark()
+    benchmark_ref.print_benchmark_results()
+
+    benchmark_ref = Elu()
+    benchmark_ref.run_benchmark()
+    benchmark_ref.print_benchmark_results()
+
+    benchmark_ref = Selu()
+    benchmark_ref.run_benchmark()
+    benchmark_ref.print_benchmark_results()
+
+    benchmark_ref = Swish()
+    benchmark_ref.run_benchmark()
+    benchmark_ref.print_benchmark_results()
 
 run_all_gluon_nn_basic_operations_benchmarks()

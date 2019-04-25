bba58a3f7bd001ee94b6a7973c983c1e8e8d971d
Sandeep Krishnamurthy <sandeep.krishna98@gmail.com>
2019-04-24 19:52:15 -0700

    Add Gluon SoftmaxCrossEntropy loss layer benchmarks

--- mxnet_benchmarks/nn_operations/loss_operations.py
+++ mxnet_benchmarks/nn_operations/loss_operations.py
@@ -165,6 +165,60 @@ class SigmoidBinaryCrossEntropyLoss(MXNetOperatorBenchmarkBase):
         self.results["MX_Gluon_Imperative_SigmoidBinaryCrossEntropyLoss_Forward_Time"] = exe_time / self.runs
 
 
+class SoftmaxCrossEntropyLoss(MXNetOperatorBenchmarkBase):
+    """Helps to benchmark Gluon SoftmaxCrossEntropyLoss Block.
+
+    By default, benchmarks both forward pass on the SoftmaxCrossEntropyLoss block on pred tensor of shape
+    (32, 1000, 1) and Label of shape (32, 1000,1) to mimic batch_size=32 and 1000 class classification.
+    By default, use weight=1.0 just to force the weight computation.
+    By default, uses 'float32' precision.
+
+    """
+
+    def __init__(self, ctx=mx.cpu(), warmup=10, runs=50, inputs=None):
+        # Set the default Inputs.
+        # Default prediction is (32, 1000, 1) to mimic a prediction of batch_size=32 and 1000 class classification.
+        if inputs is None:
+            inputs = {"pred": (32, 1000, 1),
+                      "pred_initializer": nd.normal,
+                      "label": (32, 1000, 1),
+                      "label_initializer": nd.normal,
+                      "weight": 1.0,
+                      "axis": -1,
+                      "sparse_label": False,
+                      "run_backward": False,
+                      "dtype": "float32"}
+
+        super().__init__(ctx=ctx, warmup=warmup, runs=runs, inputs=inputs)
+
+        # Create a random prediction and label tensor
+        self.pred = get_mx_ndarray(ctx=self.ctx, in_tensor=self.inputs["pred"],
+                                   dtype=self.inputs["dtype"],
+                                   initializer=self.inputs["pred_initializer"],
+                                   attach_grad=self.inputs["run_backward"])
+        self.label = get_mx_ndarray(ctx=self.ctx, in_tensor=self.inputs["label"],
+                                    dtype=self.inputs["dtype"],
+                                    initializer=self.inputs["label_initializer"],
+                                    attach_grad=self.inputs["run_backward"])
+
+        self.block = loss.SoftmaxCrossEntropyLoss(axis=self.inputs["axis"],
+                                                  sparse_label=self.inputs["sparse_label"],
+                                                  from_logits=False,
+                                                  weight=self.inputs["weight"],
+                                                  batch_axis=0)
+
+        self.block.initialize(ctx=self.ctx)
+
+    def run_benchmark(self):
+        # Warm up, ignore execution time value
+        _, _ = block_forward_and_time(block=self.block, runs=self.warmup, pred=self.pred, label=self.label)
+
+        # Run Benchmarks
+        exe_time, _ = block_forward_and_time(block=self.block, runs=self.runs, pred=self.pred, label=self.label)
+
+        self.results["MX_Gluon_Imperative_SoftmaxCrossEntropyLoss_Forward_Time"] = exe_time / self.runs
+
+
 # Utilities
 def run_all_gluon_nn_loss_operations_benchmarks():
     """Helper to run all Gluon Loss Layer benchmarks. Just runs the benchmarks with default input values.
@@ -181,6 +235,10 @@ def run_all_gluon_nn_loss_operations_benchmarks():
     benchmark_ref.print_benchmark_results()
 
     benchmark_ref = SigmoidBinaryCrossEntropyLoss()
+    benchmark_ref.run_benchmark()
+    benchmark_ref.print_benchmark_results()
+
+    benchmark_ref = SoftmaxCrossEntropyLoss()
     benchmark_ref.run_benchmark()
     benchmark_ref.print_benchmark_results()
 

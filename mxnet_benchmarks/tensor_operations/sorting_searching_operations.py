aa17b91d66216eaf738863b8248863a5e19400fc
Sandeep Krishnamurthy <sandeep.krishna98@gmail.com>
2019-04-25 17:25:37 -0700

    Add ArgMin operator benchmarks

--- mxnet_benchmarks/tensor_operations/sorting_searching_operations.py
+++ mxnet_benchmarks/tensor_operations/sorting_searching_operations.py
@@ -179,6 +179,45 @@ class ArgMax(MXNetOperatorBenchmarkBase):
         self.results["MX_ArgMax_Forward_Backward_Time"] = exe_time / self.runs
 
 
+class ArgMin(MXNetOperatorBenchmarkBase):
+    """Helps to Benchmark Tensor ArgMin operation.
+
+    By default, benchmark forward ArgMin operation on a 1024*1000 tensor on last axis.
+    Result being the index of min element in the last axis.
+
+    By default, uses precision - 'float32' for tensors.
+
+    """
+
+    def __init__(self, ctx=mx.cpu(), warmup=10, runs=50, inputs=None):
+        # Set the default Inputs
+        if inputs is None:
+            inputs = {"data": (1024, 1000),
+                      "initializer": nd.normal,
+                      "axis": -1,
+                      "keepdims": False,
+                      "run_backward": False,
+                      "dtype": "float32"}
+
+        super().__init__(ctx=ctx, warmup=warmup, runs=runs, inputs=inputs)
+
+        self.data = get_mx_ndarray(ctx=self.ctx, in_tensor=self.inputs["data"],
+                                   dtype=self.inputs["dtype"],
+                                   initializer=self.inputs["initializer"],
+                                   attach_grad=self.inputs["run_backward"])
+
+    def run_benchmark(self):
+        # Warm up, ignore execution time value
+        _, _ = nd_forward_and_time(F=nd.argmin, runs=self.warmup, data=self.data, axis=self.inputs["axis"],
+                                   keepdims=self.inputs["keepdims"])
+
+        # Run Benchmarks
+        exe_time, _ = nd_forward_and_time(F=nd.argmin, runs=self.runs, data=self.data, axis=self.inputs["axis"],
+                                          keepdims=self.inputs["keepdims"])
+
+        self.results["MX_ArgMin_Forward_Backward_Time"] = exe_time / self.runs
+
+
 # Utilities
 def run_all_sort_and_search_operations_benchmarks():
     """Helper to run all Sort and Search operator benchmarks. Just runs the benchmarks with default input values.
@@ -199,6 +238,10 @@ def run_all_sort_and_search_operations_benchmarks():
     benchmark_ref.print_benchmark_results()
 
     benchmark_ref = ArgMax()
+    benchmark_ref.run_benchmark()
+    benchmark_ref.print_benchmark_results()
+
+    benchmark_ref = ArgMin()
     benchmark_ref.run_benchmark()
     benchmark_ref.print_benchmark_results()
 

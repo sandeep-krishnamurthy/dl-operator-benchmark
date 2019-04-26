df25763c1fd20bfc272853c36d8d56f5c33e5733
Sandeep Krishnamurthy <sandeep.krishna98@gmail.com>
2019-04-25 17:19:14 -0700

    Add Topk operator benchmarks

--- mxnet_benchmarks/tensor_operations/sorting_searching_operations.py
+++ mxnet_benchmarks/tensor_operations/sorting_searching_operations.py
@@ -2,7 +2,7 @@ import mxnet as mx
 import mxnet.ndarray as nd
 
 from mxnet_benchmarks.MXNetOperatorBenchmark import MXNetOperatorBenchmarkBase
-from mxnet_benchmarks.utils.ndarray_utils import get_mx_ndarray, nd_forward_backward_and_time
+from mxnet_benchmarks.utils.ndarray_utils import get_mx_ndarray, nd_forward_and_time
 
 """ Performance benchmark tests for MXNet NDArray Sorting and Searching Operations
 1. sort
@@ -16,14 +16,15 @@ TODO:
 1. Sort and Argsort
     1.1 Descending Order
     1.2 Flatten and sort
-2. 
+2. TopK
+    1.1 K being a very small number (ex: 1) on a axis with 1000 values.
 """
 
 
 class Sort(MXNetOperatorBenchmarkBase):
     """Helps to Benchmark Tensor Sort operation.
 
-    By default, benchmark both forward and backward Sort operation on a 1024*1000 tensor on last axis in ascending
+    By default, benchmark forward Sort operation on a 1024*1000 tensor on last axis in ascending
     order.
     By default, uses precision - 'float32' for tensors.
 
@@ -36,7 +37,7 @@ class Sort(MXNetOperatorBenchmarkBase):
                       "initializer": nd.normal,
                       "axis": -1,
                       "is_ascend": True,
-                      "run_backward": True,
+                      "run_backward": False,
                       "dtype": "float32"}
 
         super().__init__(ctx=ctx, warmup=warmup, runs=runs, inputs=inputs)
@@ -48,20 +49,20 @@ class Sort(MXNetOperatorBenchmarkBase):
 
     def run_benchmark(self):
         # Warm up, ignore execution time value
-        _, _ = nd_forward_backward_and_time(F=nd.sort, runs=self.warmup, data=self.data, axis=self.inputs["axis"],
-                                            is_ascend=self.inputs["is_ascend"])
+        _, _ = nd_forward_and_time(F=nd.sort, runs=self.warmup, data=self.data, axis=self.inputs["axis"],
+                                   is_ascend=self.inputs["is_ascend"])
 
         # Run Benchmarks
-        exe_time, _ = nd_forward_backward_and_time(F=nd.sort, runs=self.runs, data=self.data, axis=self.inputs["axis"],
-                                                   is_ascend=self.inputs["is_ascend"])
+        exe_time, _ = nd_forward_and_time(F=nd.sort, runs=self.runs, data=self.data, axis=self.inputs["axis"],
+                                          is_ascend=self.inputs["is_ascend"])
 
-        self.results["MX_Sort_Forward_Backward_Time"] = exe_time / self.runs
+        self.results["MX_Sort_Forward_Time"] = exe_time / self.runs
 
 
 class ArgSort(MXNetOperatorBenchmarkBase):
     """Helps to Benchmark Tensor ArgSort operation.
 
-    By default, benchmark both forward and backward ArgSort operation on a 1024*1000 tensor on last axis in ascending
+    By default, benchmark forward ArgSort operation on a 1024*1000 tensor on last axis in ascending
     order.
     By default, uses precision - 'float32' for tensors.
 
@@ -74,7 +75,7 @@ class ArgSort(MXNetOperatorBenchmarkBase):
                       "initializer": nd.normal,
                       "axis": -1,
                       "is_ascend": True,
-                      "run_backward": True,
+                      "run_backward": False,
                       "dtype": "float32"}
 
         super().__init__(ctx=ctx, warmup=warmup, runs=runs, inputs=inputs)
@@ -86,15 +87,57 @@ class ArgSort(MXNetOperatorBenchmarkBase):
 
     def run_benchmark(self):
         # Warm up, ignore execution time value
-        _, _ = nd_forward_backward_and_time(F=nd.argsort, runs=self.warmup, data=self.data, axis=self.inputs["axis"],
-                                            is_ascend=self.inputs["is_ascend"])
+        _, _ = nd_forward_and_time(F=nd.argsort, runs=self.warmup, data=self.data, axis=self.inputs["axis"],
+                                   is_ascend=self.inputs["is_ascend"])
 
         # Run Benchmarks
-        exe_time, _ = nd_forward_backward_and_time(F=nd.argsort, runs=self.runs, data=self.data,
-                                                   axis=self.inputs["axis"],
-                                                   is_ascend=self.inputs["is_ascend"])
+        exe_time, _ = nd_forward_and_time(F=nd.argsort, runs=self.runs, data=self.data,
+                                          axis=self.inputs["axis"],
+                                          is_ascend=self.inputs["is_ascend"])
 
-        self.results["MX_ArgSort_Forward_Backward_Time"] = exe_time / self.runs
+        self.results["MX_ArgSort_Forward_Time"] = exe_time / self.runs
+
+
+class TopK(MXNetOperatorBenchmarkBase):
+    """Helps to Benchmark Tensor TopK operation.
+
+    By default, benchmark forward TopK operation with K=10, on a 1024*1000 tensor on last axis,
+    in ascending order. Result being the index of element.
+    By default, uses precision - 'float32' for tensors.
+
+    """
+
+    def __init__(self, ctx=mx.cpu(), warmup=10, runs=50, inputs=None):
+        # Set the default Inputs
+        if inputs is None:
+            inputs = {"data": (1024, 1000),
+                      "initializer": nd.normal,
+                      "axis": -1,
+                      "k": 10,
+                      "ret_typ": "indices",
+                      "is_ascend": True,
+                      "run_backward": False,
+                      "dtype": "float32"}
+
+        super().__init__(ctx=ctx, warmup=warmup, runs=runs, inputs=inputs)
+
+        self.data = get_mx_ndarray(ctx=self.ctx, in_tensor=self.inputs["data"],
+                                   dtype=self.inputs["dtype"],
+                                   initializer=self.inputs["initializer"],
+                                   attach_grad=self.inputs["run_backward"])
+
+    def run_benchmark(self):
+        # Warm up, ignore execution time value
+        _, _ = nd_forward_and_time(F=nd.topk, runs=self.warmup, data=self.data, axis=self.inputs["axis"],
+                                   k=self.inputs["k"], ret_typ=self.inputs["ret_typ"],
+                                   is_ascend=self.inputs["is_ascend"])
+
+        # Run Benchmarks
+        exe_time, _ = nd_forward_and_time(F=nd.topk, runs=self.runs, data=self.data, axis=self.inputs["axis"],
+                                          k=self.inputs["k"], ret_typ=self.inputs["ret_typ"],
+                                          is_ascend=self.inputs["is_ascend"])
+
+        self.results["MX_TopK_Forward_Backward_Time"] = exe_time / self.runs
 
 
 # Utilities
@@ -109,6 +152,10 @@ def run_all_sort_and_search_operations_benchmarks():
     benchmark_ref.print_benchmark_results()
 
     benchmark_ref = ArgSort()
+    benchmark_ref.run_benchmark()
+    benchmark_ref.print_benchmark_results()
+
+    benchmark_ref = TopK()
     benchmark_ref.run_benchmark()
     benchmark_ref.print_benchmark_results()
 
